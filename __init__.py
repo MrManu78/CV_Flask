8307104f73405f07e0bbc42820ad0c79765f969e
MrManu78 <149469053+MrManu78@users.noreply.github.com>
2024-02-27 12:12:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ def resume_template():
     return render_template("resume_template.html")    
 # Création d'une nouvelle route pour la lecture de la BDD
 
-@app.route("/consultation/")
+@app.route("/consultation")
 def ReadBDD():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
